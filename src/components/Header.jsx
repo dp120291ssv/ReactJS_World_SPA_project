9e8e04cf4843fb4dac06b143dc0ab79bfc2ed43b
Sergei Semenov <dp120291ssv@gmail.com>
2022-08-01 14:53:30 +0300

    add header / search / dropdown

---
+++ src/components/Header.jsx
@@ -0,0 +1,70 @@
+import React, {useEffect, useState} from 'react';
+import styled from 'styled-components';
+import {IoMoon, IoMoonOutline} from "react-icons/io5";
+import {Container} from './Container';
+
+const HeaderEl = styled.header`
+    box-shadow: var(--shadow);
+    background-color: var(--colors-ui-base);
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem 0;
+`;
+
+const Title = styled.a.attrs({
+    href: '/'
+})`
+    color: var(--colors-text);
+    font-size: var(--fs-sm);
+    text-decoration: none;
+    font-weight: var(--fw-bold);
+`;
+
+const ModeSwitcher = styled.div`
+    color: var(--colors-text);
+    font-size: var(--fs-sm);
+    cursor: pointer;
+    text-transform: capitalize;
+`;
+
+const TitleText = styled.span`
+    margin-left: 0.75rem;
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Chrome/Safari/Opera */
+    -moz-user-select: none; /* Firefox */
+    user-select: none;
+`;
+
+const Header = () => {
+    const [theme, setTheme] = useState('light')
+
+    const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+    }, [theme])
+
+    return (
+        <HeaderEl>
+            <Container>
+                <Wrapper>
+                    <Title>World countries</Title>
+                    <ModeSwitcher onClick={toggleTheme}>
+                        {theme === 'light' ? (
+                            <IoMoonOutline size="14px"/>
+                        ) : (
+                            <IoMoon size="14px"/>
+                        )}
+                        <TitleText>{theme} Theme</TitleText>
+                    </ModeSwitcher>
+                </Wrapper>
+            </Container>
+        </HeaderEl>
+    );
+};
+
+export default Header;
